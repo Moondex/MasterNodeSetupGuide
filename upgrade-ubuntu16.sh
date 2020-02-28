24a68b5c00727cb10be49483caf34f23dcbd9a87
CryptoWhizzard <36404435+cryptowhizzard@users.noreply.github.com>
2020-02-28 12:11:49 +0100

    Upgrade script for masternodes on Ubuntu16

---
+++ upgrade-ubuntu16.sh
@@ -0,0 +1,31 @@
+
+
+echo ""
+echo "=================================================================="
+echo "Updating MoonDEX VPS Masternode on Ubuntu 16"
+echo "Please wait..."
+echo "=================================================================="
+echo ""
+
+WALLET_VERSION='3.0'
+
+~/moondex/moondex-cli stop
+sleep 20
+wget https://github.com/Moondex/MoonDEXCoin/releases/download/3.0/linux-no-gui-v${WALLET_VERSION}.tar.gz
+tar -zxvf linux-no-gui-v${WALLET_VERSION}.tar.gz -C moondex
+~/moondex/moondexd
+echo "Syncing blockchain..."
+sleep 120
+~/moondex/moondex-cli masternode status
+
+echo ""
+echo "=================================================================="
+echo "Done!"
+echo "If you can see \"status\": \"Not capable masternode: Masternode in NEW_START_REQUIRED state\""
+echo "or \"status\": \"Masternode successfully started\" above,"
+echo "go to your hot wallet on PC/MAC to restart the masternode."
+echo "Otherwise wait till the blockchain is fully synced. Then run:"
+echo "~/moondex/moondex-cli masternode status"
+echo "to check again."
+echo "=================================================================="
+echo ""
