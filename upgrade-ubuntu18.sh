f296fa9527072c0b32d7908770e2bee75d9ad646
CryptoWhizzard <36404435+cryptowhizzard@users.noreply.github.com>
2020-03-02 21:18:16 +0100

    Upgrade version

--- upgrade-ubuntu18.sh
+++ upgrade-ubuntu18.sh
@@ -11,7 +11,7 @@ WALLET_VERSION='3.1'
 
 ~/moondex/moondex-cli stop
 sleep 20
-wget https://github.com/Moondex/MoonDEXCoin/releases/download/3.0/ubuntu18-linux-no-gui-v${WALLET_VERSION}.tar.gz
+wget https://github.com/Moondex/MoonDEXCoin/releases/download/3.1/ubuntu18-linux-no-gui-v${WALLET_VERSION}.tar.gz
 tar -zxvf ubuntu18-linux-no-gui-v${WALLET_VERSION}.tar.gz -C moondex
 ~/moondex/moondexd
 echo "Syncing blockchain..."
